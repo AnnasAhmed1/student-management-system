3c750c66097ef9a8080100cab44ded9ada9a44b2
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-01-23 21:02:08 +0500

    course-history-data-add

--- src/components/AcademicTable.jsx
+++ src/components/AcademicTable.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import DataTable from 'react-data-table-component';
+import React from "react";
+import DataTable from "react-data-table-component";
 
 const columns = [
   {
-    name: 'Class',
-    selector: row => row.class,
+    name: "Class",
+    selector: (row) => row.class,
     sortable: true,
   },
   {
-    name: 'Description',
-    selector: row => row.description,
+    name: "Description",
+    selector: (row) => row.description,
     sortable: true,
   },
   {
-    name: 'Term',
-    selector: row => row.term,
+    name: "Term",
+    selector: (row) => row.term,
     sortable: true,
   },
   {
-    name: 'Grade',
-    selector: row => row.grade,
+    name: "Grade",
+    selector: (row) => row.grade,
     sortable: true,
   },
   {
-    name: 'Units',
-    selector: row => row.units,
+    name: "Units",
+    selector: (row) => row.units,
     sortable: true,
   },
   {
-    name: 'Status',
-    cell: row => (
-      <div style={{ textAlign: 'center' }}>
-        <div style={{ display: 'inline-block', color: row.status === 'Taken' ? 'green' : 'red' }}>
+    name: "Status",
+    cell: (row) => (
+      <div style={{ textAlign: "center" }}>
+        <div
+          style={{
+            display: "inline-block",
+            color: row.status === "Taken" ? "green" : "red",
+          }}
+        >
           {row.status}
         </div>
       </div>
@@ -41,101 +46,54 @@ const columns = [
 
 const data = [
   {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
+    class: "MTH 240",
+    description: "Computer Programming Fundamentals",
+    term: "Winter 2024",
+    grade: "A",
+    units: "1.00",
+    status: "Taken",
   },
   {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
+    class: "CPS 188",
+    description: "Computer Programming Fundamentals",
+    term: "Winter 2024",
+    grade: "A",
+    units: "1.00",
+    status: "Taken",
   },
   {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
+    class: "PCS 125",
+    description: "Computer Programming Fundamentals",
+    term: "Winter 2024",
+    grade: "A",
+    units: "1.00",
+    status: "Taken",
   },
   {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
+    class: "CPS 188",
+    description: "Computer Programming Fundamentals",
+    term: "Winter 2024",
+    grade: "A",
+    units: "1.00",
+    status: "Taken",
   },
   {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
+    class: "ELE 202",
+    description: "Computer Programming Fundamentals",
+    term: "Winter 2024",
+    grade: "A",
+    units: "1.00",
+    status: "Taken",
   },
   {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
+    class: "FRE 101",
+    description: "Computer Programming Fundamentals",
+    term: "Winter 2024",
+    grade: "A",
+    units: "1.00",
+    status: "Taken",
   },
-  {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
-  },
-  {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
-  },
-  {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
-  },
-  {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
-  },
-  {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
-  },
-  {
-    class: 'CPS 188',
-    description: 'Computer Programming Fundamentals',
-    term: 'Winter 2024',
-    grade: 'A',
-    units: '1.00',
-    status: 'Taken',
-  },
+
   // ...more rows
 ];
 // const data = [
@@ -239,16 +197,15 @@ const data = [
 // ];
 
 const customStyles = {
-  
   headCells: {
     style: {
-      backgroundColor: '#f8f8f8',
-      fontWeight: 'bold',
+      backgroundColor: "#f8f8f8",
+      fontWeight: "bold",
     },
   },
   cells: {
     style: {
-      fontSize: '14px',
+      fontSize: "14px",
     },
   },
 };
