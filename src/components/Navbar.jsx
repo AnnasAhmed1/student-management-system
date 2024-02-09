85913e0c549d16d6cac208fd2d2beedf27267036
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-10 01:37:08 +0500

    logo-update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,43 @@
-
 // Import necessary dependencies
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import PSm from './paragraphs/P_Sm';
-import H3 from './headings/H3';
-import house from '../assets/house.svg'
-import logo from '../assets/browse_course.png'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import PSm from "./paragraphs/P_Sm";
+import H3 from "./headings/H3";
+import house from "../assets/house.svg";
+import logo from "../assets/logo.png";
 
 const Navbar = ({ pageTitle }) => {
   // Get the current location using React Router's useLocation hook
   const location = useLocation();
 
   // Define a function to check if the current route is the home page
-  const isHomePage = () => location.pathname === '/';
+  const isHomePage = () => location.pathname === "/";
 
   return (
-    <div className='w-full bg-colorNav  py-4 '>
-      <div className='flex justify-between items-center w-[95%] mx-auto'>
+    <div className="w-full bg-colorNav">
+      <div
+        className={`flex justify-between items-center w-[95%] mx-auto ${
+          isHomePage() ? "" : "py-4"
+        }`}
+      >
         {/* Conditionally render the logo or back button based on the current page */}
         {isHomePage() ? (
-          <img src={logo} className='h-8 w-8' alt="logo" />
+          <img src={logo} className="h-16/ w-32" alt="logo" />
         ) : (
-          <Link className='text-color2 border px-5 rounded-lg border-color1' to="/">
+          <Link
+            className="text-color2 border px-5 rounded-lg border-color1"
+            to="/"
+          >
             Back to Home
           </Link>
         )}
         {/* Render the dynamic page title */}
-        <H3 styles={'text-color2'}>{isHomePage() ? 'Home' : pageTitle}</H3>
-        <div className='flex text-white space-x-4'>
+        {/* <div className="py-4"> */}
+        <H3 styles={"text-color2"}>{isHomePage() ? "Home" : pageTitle}</H3>
+        <div className="flex text-white space-x-4">
           <img src={house} alt="house" />
           <PSm>...</PSm>
+          {/* </div> */}
         </div>
       </div>
     </div>
@@ -37,4 +45,3 @@ const Navbar = ({ pageTitle }) => {
 };
 
 export default Navbar;
-
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home/Home'
-import PSm from '../components/paragraphs/P_Sm'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import PSm from "../components/paragraphs/P_Sm";
 const RootLayout = () => {
   return (
     <div>
-    <Navbar/>
-    <div className='w-full bg-gray-200 border '>
-        <div className='flex justify-between  items-center w-[95%] mx-auto'>
-          <button className='border border-colorNav rounded-lg p-1'><PSm >Student Homepage</PSm></button>
+      <Navbar />
+      <div className="w-full bg-gray-200 border ">
+        <div className="flex justify-between  items-center w-[95%] mx-auto py-2">
+          <button className="border border-colorNav rounded-lg p-1">
+            <PSm>Student Homepage</PSm>
+          </button>
           <PSm>1 of 2 </PSm>
         </div>
       </div>
-    <Routes>
-    <Route index element={<Home />} />
-    </Routes>
-
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
