8ba53dd331be7329aedca40bece5cb754cec47c1
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-09 23:54:59 +0500

    view-classes-update

--- src/components/Tab1.jsx
+++ src/components/Tab1.jsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import Accordion from './Accordion'
+import Accordion from "./Accordion";
 
 const Tab1 = () => {
   return (
-    <div><div className="flex flex-wrap">
-    <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
-      <button className="w-1/2 bg-white border mr-auto border-color3 rounded-md py-2 px-4 text-color3 hover:bg-color3 hover:text-white">
-        Printable Text
-      </button>
-    </div>
-    <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
-      <label className="flex items-center">
-        <input type="checkbox" className="form-checkbox mr-2" />
-        Show Enrolled Classes
-      </label>
-    </div>
-    <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
-      <label className="flex items-center">
-        <input type="checkbox" className="form-checkbox mr-2" />
-        Show Waitlisted Classes
-      </label>
-    </div>
-    <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
-      <label className="flex items-center">
-        <input type="checkbox" className="form-checkbox mr-2" />
-        Show dropped Classes
-      </label>
-    </div>
-  </div>
-  
-  <Accordion title={'CECN 801 Principles Of Eng Economics'}/>
-  
-  <Accordion title={'CMTH 108 Linear Algebra'}/>
-  </div>
-  )
-}
+    <div>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
+          <button className="w-1/2 bg-white border mr-auto border-color3 rounded-md py-2 px-4 text-color3 hover:bg-color3 hover:text-white">
+            Printable Text
+          </button>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
+          <label className="flex items-center">
+            <input type="checkbox" className="form-checkbox mr-2" />
+            Show Enrolled Classes
+          </label>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
+          <label className="flex items-center">
+            <input type="checkbox" className="form-checkbox mr-2" />
+            Show Waitlisted Classes
+          </label>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
+          <label className="flex items-center">
+            <input type="checkbox" className="form-checkbox mr-2" />
+            Show dropped Classes
+          </label>
+        </div>
+      </div>
 
-export default Tab1
+      <Accordion title={"MTH 240 Calculus II"} />
+
+      <Accordion title={"CPS 188 Computer Prog. Fundamentals"} />
+      <Accordion title={"PCS 125 Physics: Waves and Fields"} />
+      <Accordion title={"ELE 202 Electircal Circuit II"} />
+      <Accordion title={"FRE 101 French"} />
+    </div>
+  );
+};
+
+export default Tab1;
